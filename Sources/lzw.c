2d6ae7b394ae2ac997f2fb0f8d0d2183569dde89
Kawatwist <masse.loic2@hotmail.com>
2020-01-30 20:52:28 +0100

    Uncompress set

--- Sources/lzw.c
+++ Sources/lzw.c
@@ -6,7 +6,7 @@
 /*   By: lomasse <lomasse@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/29 18:56:39 by lomasse           #+#    #+#             */
-/*   Updated: 2020/01/30 04:26:01 by lomasse          ###   ########.fr       */
+/*   Updated: 2020/01/30 20:52:12 by lomasse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,42 +15,19 @@
 /*
 **	Create a type of 9bits len
 */
-/*
-**	The New type will be used to reach 9 per 9 bits the src string
-**
-**	/!\ The dest string need to be malloced before to be used
-**	(Width * Height)
-*/
-// int			compress_lzw(t_lzw *src, char *dest, long len, int nb)
-// {
-// 	long	index;
-// 	char	*(dictionnary[256]);
-// 	char	pos;
-
-// 	index = -1;
-// 	pos = 0;
-// 	dictionnary[0] = NULL;
-// 	while (index < len)
-// 	{
-// 		/*
-// 		**	Check si 0b100000000 de value est actif
-// 		**	To know if the vale is a reference to the dictionary
-// 		*/
-// 		if (!(src[index].type) && nb)
-// 		{
-// 			if ((dictionnary[(int)pos] = malloc(sizeof(char) * 2)) == NULL)
-// 				return (1);
-// 			for (int i = 0; i < 2 ; i++)
-// 				dictionnary[(int)pos] = src[index].value;
-// 			pos += 1;
-// 			nb -= 1;
-// 			dictionnary[(int pos)] = NULL;
-// 		}
-// 	}
-// 	return (0);
-// }
 
 #include <stdio.h>
+
+void		show_one_dico(t_lzw dico[256])
+{
+	int	i;
+
+	i = -1;
+	while (dico[++i].value != END)
+		printf("%c", dico[i].value);
+	printf("]\n");
+}
+
 void		show_dico(t_lzw dico[256][256], int nb)
 {
 	int i;
@@ -60,60 +37,114 @@ void		show_dico(t_lzw dico[256][256], int nb)
 	while (++i < nb)
 	{
 		j = -1;
-		printf("Index [%d]:\n", i);
+		printf("Index [%d]:\t[", i);
 		while (dico[i][++j].value != END)
 			printf("%c", dico[i][j].value);
-		printf("\n");
+		printf("]\n");
 	}
 }
 
-int			add_dico(t_lzw all[256][256], t_lzw *dico, t_lzw *str, int len)
+/*
+**	Check if the current word already exist in the dictionary
+*/
+int			in_dico(t_lzw dico[256][256], t_lzw *src, int len, int max)
 {
+	int nb;
 	int	i;
-	int	pos;
-	int	tmp;
 
-	i = -1;
-	tmp = 0;
-	while (++i < len)
+	nb = 0;
+	while (nb < max)
 	{
-		if (str[i].value < LZW)
-			dico[i + tmp].value = str[i].value;
+		i = 0;
+		while (src[i].value == dico[nb][i].value && i < len)
+			i++;
+
+		if (i == len - 1)
+			return (1);
+		else
+			nb++;
+	}
+	return (0);
+}
+
+/*
+**	Add The a new word to the dictionary
+*/
+
+int			add_dico(t_lzw dico[256][256], t_lzw *src, int nb)
+{
+	int i;
+	int	tmp2;
+	int	tmp;
+	int	len;
+
+	i = 0;
+	tmp2 = 0;
+	len = -1;
+	while (src[i - 1].value != END && ++len < 2)
+	{
+		if (src[len].value > END)
+		{
+			tmp = src[len].value - 257;
+			tmp2 = 0;
+			if (tmp < nb && in_dico(dico, dico[nb], i + 1, nb - 1))
+			{
+				while (tmp < nb && in_dico(dico, dico[nb], i + 1, nb - 1))
+					dico[nb][i++].value = dico[tmp][tmp2++].value;
+				if (!in_dico(dico, dico[nb], i, nb - 1))
+					dico[nb][i].value = dico[tmp][tmp2++].value;
+			}
+			else if (!in_dico(dico, dico[nb], i + 1, nb - 1))
+				dico[nb][i++].value = dico[tmp][tmp2++].value;
+		}
 		else
 		{
-			pos = -1;
-			while (all[str[i].value - 257][++pos].value != END)
-			{
-				printf("[%c]\n", all[str[i].value - 257][++pos].value);
-				dico[i + tmp].value = all[str[i].value - 257][++pos].value;
-				tmp += pos;
-			}
+			dico[nb][i].value = src[i].value;
+			i++;
 		}
-
 	}
-	dico[i].value = END;
+	dico[nb][i].value = END;
 	return (0);
 }
 
-int			set_dico(t_lzw dico[256][256], t_lzw *str, int nb)
+/*
+**	Fill The Dest's String
+*/
+
+int			place_dest(t_lzw dico[256][256], t_lzw *src, unsigned char *dest)
 {
-	int		i;
-	int		post;
+	int	i;
+
+	i = 0;
+	if (src[0].value < END)
+		dest[0] = src[0].value;
+	else if (src[0].value > END)
+	{
+		i = -1;
+		while (dico[src[0].value - 257][++i].value != END)
+			dest[i] = dico[src[0].value - 257][i].value;
+		i -= 1;
+	}
+	return (i + 1);
+}
+
+/*
+**	Init Dictionary
+*/
+
+void		clear_dico(t_lzw dico[256][256])
+{
+	int	i;
+	int	j;
 
 	i = -1;
-	post = 1;
-	while (++i < nb)
+	while (++i < 256)
 	{
-		if (!cmpnstr(dico[i], &str[-post], post + 1)) // CHECK IF EXIST
-		{
-			post++;
-			i = -1;
-		}
+		j = -1;
+		while (++j < 256)
+			dico[i][j].value = END;
 	}
-	add_dico(dico, dico[nb], &(str[-post]), post + 1);
-	return (0);
 }
-
 
 int			uncompress_lzw(t_lzw *src, unsigned char *dest, int nb)
 {
@@ -121,12 +152,12 @@ int			uncompress_lzw(t_lzw *src, unsigned char *dest, int nb)
 	int				dico;
 	int				index;
 	int				index2;
-	int				pos;
 
 	(void)nb;
-	index = 0;
-	dico = 0;
-	pos = 1;
+/*
+**	Wikipedia Example
+*/
+/*
 	src[0].value = 'T';
 	src[1].value = 'O';
 	src[2].value = 'B';
@@ -144,35 +175,22 @@ int			uncompress_lzw(t_lzw *src, unsigned char *dest, int nb)
 	src[14].value = 262;
 	src[15].value = 264;
 	src[16].value = END;
+*/
+	index = 0;
+	index2 = 1;
+	dico = 0;
+	clear_dico(dictionary);
 	dest[0] = src[0].value;
 	while (src[++index].value != END)
 	{
-		printf("%d\n Dest : \t", src[index].value);
-		for (int i = 0; dest[i];i++)
-			printf("%c", dest[i]);
-		printf("\n");
-		if (!set_dico(dictionary, &(src[index]), dico))
-			dico++;
-		if (src[index].value > LZW)
-		{
-			index2 = -1;
-			while (dictionary[src[index].value - 257][++index2].value != END)
-			{
-				printf("pos : %d\n", pos);
-				dest[pos] = (dictionary[src[index].value - 257][index2].value & 0xFFFFFFFF);
-				pos++;
-			}
-		}
-		else
-		{
-			dest[pos] = src[index].value;
-			pos++;
-		}
-
+		if (dico < 256)
+			add_dico(dictionary, &(src[index - 1]), dico);
+		index2 += place_dest(dictionary, &(src[index]), &(dest[index2]));
+		dico++;
 	}
+	printf("Resultat : ");
 	for (int i = 0; dest[i];i++)
 		printf("%c", dest[i]);
 	printf("\n");
-	show_dico(dictionary, 9);
 	return (0);
 }
